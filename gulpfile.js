d2fec6fe7546e46ed1741cbe6f66307ecf83e5dc
ivan <murza-zhut@mail.ru>
2017-07-06 19:32:23 +0300

    add axios
    add gulp main

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,40 @@
-import gulp from 'gulp';
-import del from 'del';
-/*
- * For small tasks you can use arrow functions and export
- */
-const clean = () => del(['assets']);
-export { clean };
+'use strict';
 
-gulp.task('default', clean);
+const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
+const config = require('./build/config.js');
+const packs = require('require-dir')('./build/tasks');
+const packsKeys = Object.keys(packs);
+let tasks = [];
+
+for (let i=0; i < packsKeys.length; i++){
+    let packTasks = packs[packsKeys[i]](gulp, config);
+
+    for (let j=0; j < packTasks.length; j++){
+        tasks.push(packTasks[j]);
+    }
+}
+
+gulp.task('watch', function(){
+    gulp.watch(config.allFiles, gulp.series(tasks));
+});
+
+gulp.task('server', function() {
+    nodemon({
+        script: 'start'
+        ,watch: ['start', './back/**/*.*']
+        //ext: 'js html',
+        //,tasks: ['watch']
+        ,env: { 'NODE_ENV': 'development' }
+    }).on('start', ['watch'])
+        .on('change', ['watch'])
+        .on('restart', () => {
+            console.log('server has been restarted');
+            //gulp.src('./server.js')
+            //	.pipe(notify('Running the start tasks'));
+        });
+});
+
+tasks.push('server');
+
+gulp.task('default', gulp.series(tasks));
